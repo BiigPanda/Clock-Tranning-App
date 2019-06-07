3373bd326a699f4fac421061ded18cc61ca5620b
Marc Gallardo <marc.gallardo@inqbarna.com>
2019-06-07 19:41:06 +0200

    Initial Commit

---
+++ Clock Tranning App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clock Tranning App
+//
+//  Created by Marc Gallardo on 07/06/2019.
+//  Copyright © 2019 Marc Gallardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
